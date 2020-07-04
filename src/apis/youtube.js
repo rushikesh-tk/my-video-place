0e1e745a195fce3c295eb5bea40a38465eeb161b
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2020-07-04 16:04:51 +0530

    added axios.create() to create instance for request

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyDeC6RLTYs5IWlrn5Elzvrc362utmkTDEY';
+
+export default axios.create({
+	baseURL : "https://www.googleapis.com/youtube/v3",
+	params : {
+		part : "snippet",
+		maxResults : 10,
+		key : KEY
+	}
+});
