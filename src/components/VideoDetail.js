208bf1c8d2ba8ce0c48a4e75f0b030d2ab3b4be1
Rushikesh Taksande <rushikeshtaksande7@gmail.com>
2020-07-06 16:45:32 +0530

    added video frame

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import './VideoDetail.css';
 
 const VideoDetail = ({ video }) => {
 	if(!video){
-			return <h4>Loading...</h4>
+			return <h4>Please Wait While We Are Loading...</h4>
 	}
 
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
 	return (
-		<div className="bg-near-black pa3">
-			<div>
+		<div className="bg-near-black">
+			<div className="iframe-style">
+				<iframe title={video.snippet.title} src={videoSrc}/>
+			</div>
+			<div className="pa3">
 				<h4 className="white">{video.snippet.title}</h4>
-				<p className="hot-pink">{video.snippet.description}</p>
+				<p className="hot-pink pb4">{video.snippet.description}</p>
 			</div>
 		</div>
 	);
